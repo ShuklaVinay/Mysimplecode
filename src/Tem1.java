3c0a2b2209b6ba6c6905b223dd9ca46a1b96d248
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-12 10:33:57 +0530

    all files to be commited

---
+++ src/Tem1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Tem1
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please inter any integer no.");
+        int i=sc.nextInt();
+        System.out.println("yuo enterd int no. is :"+" " +i);
+    }
+}
