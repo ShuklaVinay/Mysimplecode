3dcc32c37922e435b37b62294d8ce6deb467a1e0
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-12 17:26:10 +0530

    add one more program print n natural no. in descending order using for loop

---
+++ src/Temp5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Temp5
+{
+    public static void main(String[] args)
+    {
+     int n,i;
+        System.out.println("enter the no. of term:");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=n;i>0;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
