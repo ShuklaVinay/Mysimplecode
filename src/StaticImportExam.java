f919adc39b826be6b76ce9c2c428db6f90c5d9be
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-13 20:44:28 +0530

    program by using static import

---
+++ src/StaticImportExam.java
@@ -0,0 +1,9 @@
+import static java.lang.System.*;
+public class StaticImportExam
+{
+    public static void main(String[] args)
+    {
+     out.println("hello this program is static import");
+     out.println("without use of System cass");
+    }
+}
